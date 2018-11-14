95a8345ca8e3d493ad07fb5158836245c868ef32
NG SING KWONG Johann <johann.ng-sing-kwong-ext@socgen.com>
2018-11-14 19:41:59 +0100

    Initiated websocket integration

--- router/router.go
+++ router/router.go
@@ -1,20 +1,45 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"	
 )
+
+var wsupgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+    conn, err := wsupgrader.Upgrade(w, r, nil)
+    if err != nil {
+		fmt.Println("Failed to set websocket upgrade: %+v", err)
+       return
+    }
+
+    for {
+        t, msg, err := conn.ReadMessage()
+        if err != nil {
+            break
+        }
+        conn.WriteMessage(t, msg)
+    }
+}
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
 
-	api := r.Group("/api")
-	{
-		api.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H {
-				"message": "pong",
-			})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H {
+			"message": "pong",
 		})
-	}
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
+	})
+
 	return r
 }
--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,7 @@
 console.log("Noughts & Crosses starting...");
 
 const render = () => {
-  const appElement = new DOMParser().parseFromString(`
+	const appElement = new DOMParser().parseFromString(`
 			<div class="game">
 				<div class="me">Ready Player One</div>
 				<div class="game-table">
@@ -17,11 +17,39 @@ const render = () => {
 				</div>
 			</div>
 				`, "text/html").body.firstChild
-  document.body.innerHTML = ''
-  document.body.appendChild(appElement)
+	document.body.innerHTML = ''
+	document.body.appendChild(appElement)
 }
 
 document.addEventListener('DOMContentLoaded', (e) => {
-  render()
+	const ws = new WebSocket("ws://localhost:3000/ws")
+	ws.onerror = () => console.log('WebSocket error')
+	ws.onopen = () => {
+		setInterval(
+			function () { ws.send("ping") }
+			, 1000)
+	}
+	ws.onclose = () => console.log('WebSocket connection closed')
+	ws.onmessage = function (event) {
+		const message = event.data
+		console.log(`Message received: ${message}`)
+		let data
+		try {
+			data = JSON.parse(message)
+		} catch (err) {
+			return
+		}
+
+		if (data.event == 'update_game_state') {
+			state = data.game_state
+			render()
+		}
+	}
+	window.onbeforeunload = function () {
+		ws.onclose = function () { }
+		ws.send('close')
+		ws.close()
+	}
+	render()
 })
 
